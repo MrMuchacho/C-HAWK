f8bed8623e5fab34af45ff719bd0e9fe8f6e9d0d
ChristianGebhardt <gebhardt.christian@campus.lmu.de>
2016-08-10 19:06:53 +0200

    Pixelbestimmung korrigiert

--- patternRecognition.py
+++ patternRecognition.py
@@ -23,21 +23,36 @@ def cornerPointsChess(img):
     
     if ret == True:
         print "Chessboard found!"
-        x1 = round(corners[0][0][0])
-        y1 = round(corners[0][0][1])
-        x2 = round(corners[NBR_COLUMNS*NBR_ROWS-1][0][0])
-        y2 = round(corners[NBR_COLUMNS*NBR_ROWS-1][0][1])
+        #Find left-top corner value && right-bottom corner value
+        xalt[0] = round(corners[0][0][0])
+        yalt[0] = round(corners[0][0][1])
+        xalt[1] = round(corners[NBR_COLUMNS-1][0][0])
+        yalt[1] = round(corners[NBR_COLUMNS-1][0][1])
+        xalt[2] = round(corners[NBR_COLUMNS*NBR_ROWS-1][0][0])
+        yalt[2] = round(corners[NBR_COLUMNS*NBR_ROWS-1][0][1])
+        xalt[3] = round(corners[(NBR_COLUMNS-1)*NBR_ROWS][0][0])
+        yalt[3] = round(corners[(NBR_COLUMNS-1)*NBR_ROWS][0][1])        
+        for i in range(0, 3):
+            sumV[i] = xalt[i] + yalt[i]
+        minV = min(sumV)
+        maxV = max(maxV)
+        for i in range(0, 3):
+            if minV==sumV:
+                x1 = xalt[i], y1 = yalt[i]
+            if maxV==sumV:
+                x2 = xalt[i], y2 = yalt[i]
+        
         print "Endpoints: ("+str(x1)+","+str(y1)+") ; ("+str(x2)+","+str(y2)+")"
     
         # Draw and display the corners (ADD FRAMES)
         cv2.drawChessboardCorners(img, (NBR_COLUMNS,NBR_ROWS), corners,ret)
         print "Print image"
-        cv2.imshow('img',img)
-        cv2.waitKey(1)
-            
-        return x1,y1,x2,y2
+
     else:
         print "Chessboard not found!"
-        cv2.imshow('img',img)
-        cv2.waitKey(1)
-        return -1,-1,-1,-1
+        x1=-1,y1=-1,x2=-1,y2=-1
+        
+    cv2.imshow('img',img)
+    cv2.waitKey(1)
+        
+    return x1,y1,x2,y2
